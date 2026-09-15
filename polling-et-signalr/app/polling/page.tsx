4c507b620efd0adbd55a3a9c9a5c8ca68bf5a490
Yohan Tremblay <2384931@cegepmontpetit.ca>
2026-09-15 10:21:42 -0400

    polling fait

--- polling-et-signalr/app/polling/page.tsx
+++ polling-et-signalr/app/polling/page.tsx
@@ -13,12 +13,17 @@ export default function Home() {
     updateTasks();
   }, []);
 
-  function handleTaskAdd(taskName: string) {
+  async function handleTaskAdd(taskName: string) {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
+    const response = await axios.post("http://localhost:5042/api/UselessTasks/Add", null, { params: { taskText: taskName } });
+    console.log(response.data);
   }
 
-  function onTaskToggle(id: number) {
+  async function onTaskToggle(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
+
+    const response = await axios.get("http://localhost:5042/api/UselessTasks/Complete/" + id);
+    console.log(response.data);
 
     let tasksCopy : UselessTask[] = [...tasks];    
     tasksCopy.find(task => task.id === id)!.completed = true;
@@ -26,12 +31,11 @@ export default function Home() {
   }
 
   async function updateTasks() {
-    let testTasks = new Array<UselessTask>(
-      { id: 1, text: "Test Task 1", completed: false },
-      { id: 2, text: "Test Task 2", completed: true });
-    setTasks(testTasks);
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
+    const response = await axios.get("http://localhost:5042/api/UselessTasks/GetAll");
+    setTasks(response.data);
     // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
+    setTimeout(() => {updateTasks()}, 1000);
   }
 
   return (
