cd69b334d09e3e36c1daf56f4013688d84633d98
Yohan-Tremblay <2384931@cegepmontpetit.ca>
2026-09-17 23:20:45 -0400

    fin du labo

--- PollingEtSignalR/labo.signalr.api/Hubs/TaskHub.cs
+++ PollingEtSignalR/labo.signalr.api/Hubs/TaskHub.cs
@@ -1,9 +1,60 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using labo.signalr.api.Data;
+using labo.signalr.api.Migrations;
+using labo.signalr.api.Models;
+using Microsoft.AspNetCore.SignalR;
 
 namespace labo.signalr.api.Hubs
 {
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+    }
+
     public class TaskHub : Hub
     {
+        private ApplicationDbContext _context;
 
+        public TaskHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("UserCount", UserHandler.ConnectedIds.Count);
+            await Clients.Caller.SendAsync("TaskList", _context.UselessTasks.ToList());
+        }
+
+        public async Task AddTask(string task)
+        {
+            UselessTask uselessTask = new UselessTask()
+            {
+                Completed = false,
+                Text = task
+            };
+            _context.UselessTasks.Add(uselessTask);
+            await _context.SaveChangesAsync();
+            await Clients.All.SendAsync("TaskList", _context.UselessTasks.ToList());
+        }
+
+        public async Task CompleteTask(int id)
+        {
+            UselessTask? task = await _context.FindAsync<UselessTask>(id);
+            if (task != null)
+            {
+                task.Completed = true;
+                await _context.SaveChangesAsync();
+            }
+            await Clients.All.SendAsync("TaskList", _context.UselessTasks.ToList());
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("UserCount", UserHandler.ConnectedIds.Count);
+            await base.OnDisconnectedAsync(exception);
+        }
     }
 }
--- polling-et-signalr/app/signalr/page.tsx
+++ polling-et-signalr/app/signalr/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { UselessTask } from "../models/UselessTask";
 import TaskView from "../_components/tasks-view";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 
 export default function Home() {
 
   const [tasks, setTasks] = React.useState<UselessTask[]>([]);
+  const [hubConnection, setHubConnection] = useState<HubConnection>();
+  const [nbConnexions, setNbConnexions] = useState(0);
 
   useEffect(() => {
       connecttohub();
@@ -18,20 +21,35 @@ export default function Home() {
           { id: 1, text: "Test Task 1", completed: false },
           { id: 2, text: "Test Task 2", completed: true });
         setTasks(testTasks);
+
     // TODO On doit commencer par créer la connexion vers le Hub
+    let newHubConnection = new HubConnectionBuilder().withUrl("http://localhost:5042/taskHub").build();
+
     // TODO On peut commencer à écouter pour les messages qui vont déclencher des callbacks (Il y a seulement un message pour commencer)
+    newHubConnection.on("TaskList", (data) => {
+      setTasks(data);
+    });
+
+    newHubConnection.on("UserCount", (data) =>{
+      setNbConnexions(data);
+    });
+
     // TODO On doit ensuite se connecter
+    newHubConnection.start().then(() => {
+      console.log("La connexion est active");
+    }).catch(err => console.log("Erreur : " + err));
+
+    setHubConnection(newHubConnection);
   }
 
   function onTaskToggle(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur
-    let tasksCopy : UselessTask[] = [...tasks];    
-    tasksCopy.find(task => task.id === id)!.completed = true;
-    setTasks(tasksCopy);
+    hubConnection?.invoke("CompleteTask", id);
   }
 
-  function handleTaskAdd() {
+  function handleTaskAdd(taskname: string) {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur
+    hubConnection?.invoke("AddTask", taskname);
   }
 
   return (
@@ -42,6 +60,7 @@ export default function Home() {
           onTaskAdd={handleTaskAdd}
           onTaskToggle={onTaskToggle}
         />
+        <p>nb utilisateur : {nbConnexions}</p>
     </div>
   );
 }
